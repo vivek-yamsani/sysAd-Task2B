080bf5df652853d4b90e87cd7375ee1693851e6f
vivek <vivekyamsani@gmail.com>
2021-08-06 15:00:26 +0530

    adding users

--- MoMs/making_db.sql
+++ MoMs/making_db.sql
@@ -12,6 +12,12 @@ CREATE TABLE IF NOT EXISTS MoMs(
     );
 LOAD DATA LOCAL INFILE '/var/lib/MoMs.txt' INTO TABLE MoMs
 COLUMNS TERMINATED BY '|';
+CREATE TABLE IF NOT EXISTS Users(
+    username varchar(20) ,
+    passwd  varchar(20),
+    UNIQUE (username)
+    );
+INSERT INTO Users(username, passwd) VALUES("superuser", "superuserpass");
 CREATE USER IF NOT EXISTS 'user'@'localhost' IDENTIFIED BY 'password2';
 GRANT SELECT, SHOW VIEW ON Delta.* TO 'user'@'localhost';
 FLUSH PRIVILEGES;
